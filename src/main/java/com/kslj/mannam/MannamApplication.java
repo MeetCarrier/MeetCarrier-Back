0249803b417d5cd3b9f37eed6863731dbb0dee15
son0307 <son03071@naver.com>
2025-03-18 18:13:47 +0900

    Spring Init

---
+++ src/main/java/com/kslj/mannam/MannamApplication.java
@@ -0,0 +1,13 @@
+package com.kslj.mannam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MannamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MannamApplication.class, args);
+	}
+
+}
