d996dc1856bebdda83dda77f74b478043329dbee
son0307 <son03071@naver.com>
2025-03-29 17:05:31 +0900

    enum 타입 대소문자 구분 없이 매칭되도록 변경

--- src/main/java/com/kslj/mannam/domain/user/enums/SocialType.java
+++ src/main/java/com/kslj/mannam/domain/user/enums/SocialType.java
@@ -1,6 +1,15 @@
 package com.kslj.mannam.domain.user.enums;
 
+import java.util.Arrays;
+
 public enum SocialType {
     Kakao,
-    Google,
+    Google;
+
+    public static SocialType from(String value) {
+        return Arrays.stream(SocialType.values())
+                .filter(type -> type.name().equalsIgnoreCase(value))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 소셜 타입: " + value));
+    }
 }
