88e2b54e0f18093b43c952d599510081399bc33d
son0307 <son03071@naver.com>
2025-04-02 17:02:48 +0900

    자가 테스트 dto들 추가

---
+++ src/main/java/com/kslj/mannam/domain/test/dto/TestRequestDto.java
@@ -0,0 +1,15 @@
+package com.kslj.mannam.domain.test.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class TestRequestDto {
+    int depressionScore;
+    int relationshipScore;
+}
