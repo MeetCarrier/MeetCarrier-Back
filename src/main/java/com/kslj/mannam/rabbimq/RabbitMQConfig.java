f8bafac0b7add9c2cdfc25468baee2b7d9dbd5ec
son0307 <son03071@naver.com>
2025-04-24 16:57:18 +0900

    RabbitMQ 큐 생성 부분 추가

--- src/main/java/com/kslj/mannam/rabbimq/RabbitMQConfig.java
+++ src/main/java/com/kslj/mannam/rabbimq/RabbitMQConfig.java
@@ -1,14 +1,25 @@
 package com.kslj.mannam.rabbimq;
 
+import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class RabbitConfig {
+public class RabbitMQConfig {
 
     @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter() {
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public Queue aiResponseQueue() {
+        return new Queue("ai_response_queue", true);
+    }
+
+    @Bean
+    public Queue aiRequestQueue() {
+        return new Queue("ai_request_queue", true);
     }
 }
