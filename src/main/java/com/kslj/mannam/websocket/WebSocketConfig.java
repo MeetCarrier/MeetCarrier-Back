99bc6a94b8e82385063556f64059f52777fc2ab4
son0307 <son03071@naver.com>
2025-05-27 15:29:26 +0900

    웹소켓 연결할 때도 '/api' 붙이게

--- src/main/java/com/kslj/mannam/websocket/WebSocketConfig.java
+++ src/main/java/com/kslj/mannam/websocket/WebSocketConfig.java
@@ -18,7 +18,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connection")
+        registry.addEndpoint("/api/connection")
                 .setAllowedOriginPatterns("*")
                 .withSockJS();
     }
