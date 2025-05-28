946202b6380c3b0ce084aa752629b8da0a30470d
son0307 <162094052+son0307@users.noreply.github.com>
2025-05-28 20:03:00 +0900

    웹소켓 테스트 페이지 추가

---
+++ src/main/java/com/kslj/mannam/websocket/WebSocketTestController.java
@@ -0,0 +1,13 @@
+package com.kslj.mannam.websocket;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WebSocketTestController {
+
+    @GetMapping("/websocket-test")
+    public String websocketTestPage() {
+        return "websocket-test"; // templates/websocket-test.html을 반환
+    }
+}
