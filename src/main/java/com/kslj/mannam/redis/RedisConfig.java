c260a0d909c8049ec9efdd3e6cdf4050302e8be9
son0307 <162094052+son0307@users.noreply.github.com>
2025-06-10 17:26:10 +0900

    ObjectMapper 설정으로 인해 전달되는 데이터에도 타입 붙는 오류 수정

--- src/main/java/com/kslj/mannam/redis/RedisConfig.java
+++ src/main/java/com/kslj/mannam/redis/RedisConfig.java
@@ -5,19 +5,19 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
 import com.fasterxml.jackson.datatype.hibernate6.Hibernate6Module;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.kslj.mannam.domain.user.entity.User;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
@@ -39,30 +39,43 @@ public class RedisConfig {
     }
 
     /**
-     * ObjectMapper를 하나의 빈으로 통합 관리합니다.
-     * 필요한 모든 모듈을 여기에 등록합니다.
+     * [수정] Redis 전용 ObjectMapper
+     * - 타입 정보를 포함시켜 ClassCastException을 방지합니다.
+     * - @Qualifier를 위해 "redisObjectMapper"라는 이름을 부여합니다.
      */
-    @Bean
-    public ObjectMapper objectMapper() {
+    @Bean("redisObjectMapper")
+    public ObjectMapper redisObjectMapper() {
         ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule()); // LocalDateTime 직렬화
-        objectMapper.registerModule(new Hibernate6Module()); // Lazy Loading 관련 프록시 객체 직렬화 (선택 사항)
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // 날짜를 timestamp 대신 ISO 형식으로
-
-        // 역직렬화 시 타입 정보를 명시해 ClassCastException 방지
-        objectMapper.activateDefaultTyping(
-                LaissezFaireSubTypeValidator.instance,
-                ObjectMapper.DefaultTyping.NON_FINAL
-        );
-
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Hibernate6Module());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL);
         return objectMapper;
     }
 
     /**
-     * 통합된 ObjectMapper를 사용하는 RedisTemplate
+     * [추가] API 응답 등 일반적인 용도를 위한 ObjectMapper
+     * - 타입 정보를 포함하지 않습니다.
+     * - @Primary 어노테이션으로, Spring MVC 등에서 ObjectMapper를 사용할 때 이 빈을 기본으로 사용하도록 지정합니다.
      */
     @Bean
-    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
+    @Primary
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.registerModule(new Hibernate6Module());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        return objectMapper;
+    }
+
+    /**
+     * [수정] "redisObjectMapper"를 주입받도록 @Qualifier를 사용합니다.
+     */
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(
+            RedisConnectionFactory redisConnectionFactory,
+            @Qualifier("redisObjectMapper") ObjectMapper objectMapper) { // <-- @Qualifier 추가
+
         RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
         redisTemplate.setConnectionFactory(redisConnectionFactory);
 
@@ -80,12 +93,13 @@ public class RedisConfig {
      * 통합된 ObjectMapper를 사용하는 CacheManager
      */
     @Bean
-    public CacheManager cacheManager(RedisConnectionFactory redisConnectionFactory, ObjectMapper objectMapper) {
-        Jackson2JsonRedisSerializer<User> serializer = new Jackson2JsonRedisSerializer<>(objectMapper(), User.class);
+    public CacheManager cacheManager(
+            RedisConnectionFactory redisConnectionFactory,
+            @Qualifier("redisObjectMapper") ObjectMapper objectMapper) { // <-- @Qualifier 추가
 
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(new StringRedisSerializer()))
-                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(serializer))
+                .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(new GenericJackson2JsonRedisSerializer(objectMapper)))
                 .entryTtl(Duration.ofMinutes(30L));
 
         return RedisCacheManager
